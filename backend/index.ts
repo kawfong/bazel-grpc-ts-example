1bba16632ff7ed08a7732d104c1c7608c0754bdb
Ka Wing Fong <56972746+kawfong@users.noreply.github.com>
2023-12-31 21:51:09 -0500

    chore: use path alias for auto gen clients

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,32 @@
-console.log('hello');
+import {
+    HelloReply,
+    HelloRequest,
+} from '@/auto_gen/helloworld_pb';
+
+import {GreeterService} from '@/auto_gen/helloworld_connect';
+import { ConnectRouter } from "@connectrpc/connect";
+import http from "http";
+import express from "express";
+import { expressConnectMiddleware } from "@connectrpc/connect-express";
+
+const app = express();
+const routes = (router: ConnectRouter) =>
+    router.service(GreeterService, {
+        // implements rpc sayHello
+        async sayHello(req: HelloRequest) {
+            return new HelloReply({
+                message: `Hello again, ${req.name}!`,
+            });
+        },
+        async sayHelloAgain(req) {
+            return new HelloReply({
+                message: `Hello again, ${req.name}!`,
+            });
+        },
+    });
+
+app.use(expressConnectMiddleware({
+    routes
+}));
+
+http.createServer(app).listen(8080);
